09a00e87470d788f98a06de8d4c1bcdc60932ad1
Olivier Huin <olih@users.noreply.github.com>
2026-02-10 19:28:45 +0000

    Fixes release

--- build-go.mjs
+++ build-go.mjs
@@ -29,10 +29,7 @@ const ldflags = `-X ${projectName}/cli.Version=${version} -X ${projectName}/cli.
 const platforms = [
   { label: 'Linux (amd64)', os: 'linux', arch: 'amd64' },
   { label: 'Linux (arm64)', os: 'linux', arch: 'arm64' },
-  { label: 'macOS (Intel)', os: 'darwin', arch: 'amd64' },
   { label: 'macOS (Apple Silicon)', os: 'darwin', arch: 'arm64' },
-  { label: 'Windows (amd64)', os: 'windows', arch: 'amd64' },
-  { label: 'Windows (arm64)', os: 'windows', arch: 'arm64' },
 ];
 
 await ensureDir('build');
